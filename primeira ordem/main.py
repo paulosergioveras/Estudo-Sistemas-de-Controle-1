86eb0e88c3b9c77377ff1d37757c30484860d528
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2026-05-11 18:46:50 -0300

    feat: cria funcoes para sistemas de primeira ordem

---
+++ primeira ordem/main.py
@@ -0,0 +1,11 @@
+# Constante de tempo Tc:
+def cte_tempo(a):
+    return 1/a
+
+# Tempo de subida Tr:
+def tempo_de_subida(a):
+    return 2.2/a
+
+# Tempo de acomodação Ts:
+def tempo_de_acomodacao(a):
+    return 4/a
